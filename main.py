400aefe383d73219767a12c3afefd4c5bfc543b3
jmuriki <106560016+jmuriki@users.noreply.github.com>
2022-09-28 22:33:41 +0300

    Update main.py to download image to folder

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import requests
 
+from pathlib import Path
+
 
 def main():
+	images_folder_name = 'images'
 	filename = 'hubble.jpeg'
 	url = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
 	response = requests.get(url)
 	response.raise_for_status()
-
-	with open(filename, "wb") as file:
+	Path(f"./{images_folder_name}").mkdir(parents=True, exist_ok=True)
+	with open(f"./{images_folder_name}/{filename}", "wb") as file:
 		file.write(response.content)
 
 
