de15208d8db607b4fa5ed55693ecd19659834bf2
jmuriki <106560016+jmuriki@users.noreply.github.com>
2022-09-29 01:32:03 +0300

    Add fetch_spacex_last_launch function

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 import requests
+import json
 
 from pathlib import Path
 
 
-def download_and_save_image(url, folder):
-	response = requests.get(url)
+def fetch_spacex_last_launch(images_folder_name):
+	space_x_api_url = "https://api.spacexdata.com/v5/launches/"
+	response = requests.get(space_x_api_url)
 	response.raise_for_status()
-	filename = 'hubble.jpeg'
-	with open(f"./{folder}/{filename}", "wb") as file:
-		file.write(response.content)
+	for launche in response.json()[::-1]:
+		links = launche['links']['flickr']['original']
+		if links:
+			for link_number, link in enumerate(links):
+				response = requests.get(link)
+				response.raise_for_status()
+				filename = f'spacex_{link_number}.jpg'
+				with open(f"./{images_folder_name}/{filename}", "wb") as file:
+					file.write(response.content)
+			break
 
 
 def main():
-	url = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
 	images_folder_name = 'images'
 	Path(f"./{images_folder_name}").mkdir(parents=True, exist_ok=True)
-	download_and_save_image(url, images_folder_name)
+	fetch_spacex_last_launch(images_folder_name)
 
 
 if __name__ == "__main__":
