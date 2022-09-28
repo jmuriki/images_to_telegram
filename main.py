0370f7dbedf3e65bcb6e63ebb26fd8d1a0906c27
jmuriki <106560016+jmuriki@users.noreply.github.com>
2022-09-28 22:42:14 +0300

    Add download_and_save_image function

--- main.py
+++ main.py
@@ -3,15 +3,19 @@ import requests
 from pathlib import Path
 
 
-def main():
-	images_folder_name = 'images'
-	filename = 'hubble.jpeg'
-	url = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
+def download_and_save_image(url, folder):
 	response = requests.get(url)
 	response.raise_for_status()
-	Path(f"./{images_folder_name}").mkdir(parents=True, exist_ok=True)
-	with open(f"./{images_folder_name}/{filename}", "wb") as file:
+	filename = 'hubble.jpeg'
+	with open(f"./{folder}/{filename}", "wb") as file:
 		file.write(response.content)
+
+
+def main():
+	url = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
+	images_folder_name = 'images'
+	Path(f"./{images_folder_name}").mkdir(parents=True, exist_ok=True)
+	download_and_save_image(url, images_folder_name)
 
 
 if __name__ == "__main__":
