88afcde612270c9e8c760bbf96ba96f34751a641
jmuriki <106560016+jmuriki@users.noreply.github.com>
2022-09-28 22:09:54 +0300

    Update main.py to download picture of Hubble

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+import requests
+
+
 def main():
-	pass
+	filename = 'hubble.jpeg'
+	url = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
+	response = requests.get(url)
+	response.raise_for_status()
+
+	with open(filename, "wb") as file:
+		file.write(response.content)
+
 
 if __name__ == "__main__":
 	main()
