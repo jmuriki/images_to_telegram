9d52ddfd580124709118fbd88a94c465f617df7f
Evgenii Zhmero-Gaidenko <106560016+jmuriki@users.noreply.github.com>
2022-09-30 20:28:23 +0300

    Publish image to Telegram channel

--- nasa_spacex_images_bot.py
+++ nasa_spacex_images_bot.py
@@ -10,6 +10,7 @@ def main():
 	chat_id = os.environ['CHAT_ID']
 	bot = telegram.Bot(token=token)
 	bot.send_message(text='Поехали!', chat_id=chat_id)
+	bot.send_document(chat_id=chat_id, document=open('./images/space_x/space_x_2022-04-17_0.jpg', 'rb'))
 
 
 if __name__ == "__main__":
