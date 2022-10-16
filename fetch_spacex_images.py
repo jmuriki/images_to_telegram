6ed1ade5f7029872948dc5a0e39620f35b425cfc
Evgenii Zhmero-Gaidenko <106560016+jmuriki@users.noreply.github.com>
2022-10-16 19:02:42 +0300

    Внесение незначительных правок

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -13,15 +13,15 @@ def create_parser(argument_name):
     return parser
 
 
-def get_api_response(flight_id):
+def get_spacex_response(flight_id):
 	api_url = f"https://api.spacexdata.com/v5/launches/{flight_id}"
 	response = requests.get(api_url)
 	response.raise_for_status()
 	return response.json() if not flight_id else [response.json()]
 
 
-def parse_response(api_response):
-	for day in api_response[::-1]:
+def parse_response(spacex_response):
+	for day in spacex_response[::-1]:
 		date = day["date_local"][:10]
 		links = day["links"]["flickr"]["original"]
 		if date and links:
@@ -31,21 +31,21 @@ def parse_response(api_response):
 def fetch_images(main_folder, links, date):
 	secondary_folder = create_folder_safely(main_folder, "space_x")
 	for link_number, link in enumerate(links, start=1):
-			filepath = compose_filepath(
-				link,
-				main_folder,
-				date,
-				secondary_folder,
-				link_number
-			)
-			save_content(link, filepath)
+		filepath = compose_filepath(
+			link,
+			main_folder,
+			date,
+			secondary_folder,
+			link_number
+		)
+		save_content(link, filepath)
 
 
 def main():
 	main_folder = create_folder_safely()
 	flight_id = create_parser("--id").parse_args().id
-	api_response = get_api_response(flight_id)
-	date, links = parse_response(api_response)
+	spacex_response = get_spacex_response(flight_id)
+	date, links = parse_response(spacex_response)
 	fetch_images(main_folder, links, date)
 
 
