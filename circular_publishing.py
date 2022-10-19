3f92033b10d4b34b5cca964250063407c6ffc9ec
Evgenii Zhmero-Gaidenko <106560016+jmuriki@users.noreply.github.com>
2022-10-19 12:43:35 +0300

    Доработаны функции, высушен код
    - сделана защита от некорректных входных данных в define_publication_interval
    - перенесён запуск проверки на системные файлы из get_paths (get_files_paths) в main
    - скорректированы импорты

--- circular_publishing.py
+++ circular_publishing.py
@@ -5,15 +5,16 @@ import random
 from dotenv import load_dotenv
 
 from multifunctional_module import create_folder_safely
-from multifunctional_module import get_paths
+from multifunctional_module import get_files_paths
+from multifunctional_module import find_system_files
 from multifunctional_module import publish_content
 
 
 def define_publication_interval(interval_hours):
-	if not interval_hours:
+	try:
+		return float(interval_hours) * 3600
+	except:
 		return 4 * 3600
-	else:
-		return int(float(interval_hours) * 3600)
 
 
 def main():
@@ -23,11 +24,12 @@ def main():
 	interval = os.getenv("PUBLICATION_INTERVAL", default="")
 	duration_sec = define_publication_interval(interval)
 	images_folder = create_folder_safely()
-	paths = get_paths(images_folder)
+	paths = get_files_paths(images_folder)
 	while paths:
 		for path in paths:
-			publish_content(token, chat_id, path)
-			time.sleep(duration_sec)
+			if not find_system_files(path):
+				publish_content(token, chat_id, path)
+				time.sleep(duration_sec)
 		random.shuffle(paths)
 	print("Изображений не найдено.")
 
